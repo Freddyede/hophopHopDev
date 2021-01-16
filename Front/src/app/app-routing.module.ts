852906c1f463df89744ee4e0039b70b8a09a6060
Franck <patouillardfranck3@gmail.com>
2021-01-16 09:55:08 +0100

    Create header and footer component

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
+
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent },
+
 ];
+
+// configures NgModule imports and exports
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
