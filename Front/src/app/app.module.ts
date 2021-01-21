6feca9bb8afcf6313d4882b52939abaa2b298e89
Franck <patouillardfranck3@gmail.com>
2021-01-21 20:43:48 +0100

    View all changes with 2 components.

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    ContactComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
