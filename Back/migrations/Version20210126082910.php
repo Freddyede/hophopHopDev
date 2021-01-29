bd7274766640074f133e0219d9e6ae16703768f3
Franck <patouillardfranck3@gmail.com>
2021-01-29 16:17:54 +0100

    connect Front with back

---
+++ Back/migrations/Version20210126082910.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210126082910 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user CHANGE tel tel VARCHAR(10) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649F037AB0F ON user (tel)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX UNIQ_8D93D649F037AB0F ON user');
+        $this->addSql('ALTER TABLE user CHANGE tel tel BIGINT NOT NULL');
+    }
+}
